ea2411c6552543e0836f6794f9c4dd85a0fd1c43
jesse <jessekarwaski@hotmail.com>
2025-01-20 23:02:12 -0500

    test

--- Backend/routes/webhook_routes.py
+++ Backend/routes/webhook_routes.py
@@ -4,7 +4,7 @@ webhook_bp = Blueprint('webhook', __name__)
 
 @webhook_bp.route('/webhook', methods=['POST'])
 def git_update():
-    repo = git.Repo('./DnD-Combat-app')
+    repo = git.Repo('../../')
     origin = repo.remotes.origin
     repo.create_head('main',
                      origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
