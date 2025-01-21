813a56070a378d295cf1b169f0daf3a1f3fed7dc
jesse <jessekarwaski@hotmail.com>
2025-01-20 23:40:20 -0500

    test

--- Backend/app.py
+++ Backend/app.py
@@ -5,10 +5,9 @@ from flask_migrate import Migrate
 from routes.player_routes import player_bp
 from routes.party_routes import party_bp
 from routes.monster_routes import monster_bp
-from routes.webhook_routes import webhook_bp
 from flask_migrate import Migrate
 from routes.encounter_routes import encounter_bp
-
+import git
 app = Flask(__name__)
 CORS(app)
 
@@ -26,14 +25,32 @@ app.register_blueprint(player_bp)
 app.register_blueprint(party_bp)
 app.register_blueprint(monster_bp)
 app.register_blueprint(encounter_bp)
-app.register_blueprint(webhook_bp)
+
 
 
 with app.app_context():
     db.create_all()
 
+@app.route('/git_update', methods=['POST'])
+def git_update():
+    repo = git.Repo('../')
+    origin = repo.remotes.origin
+    repo.create_head('main',
+                     origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
+    origin.pull()
+    return '', 200
+@app.route('/git_update', methods=['GET'])
+def get_repo():
+    # REPO_PATH = os.path.join(os.path.dirname(__file__), '../../')
 
-
+    try:
+        repo = git.Repo('../')
+        current_branch = repo.active_branch.name
+        print(f"Current branch: {current_branch}")
+        return f"Current branch: {current_branch}", 200  # Return branch name as response with status code 200
+    except Exception as e:
+        print(f"Error: {e}")
+        return f"An error occurred: {e}", 500  # Return error message with status code 500
 
 @app.route('/')
 def index():
