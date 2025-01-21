dc98eafdd9ebecac05dd2c0a45699cc68097fad3
jesse <jessekarwaski@hotmail.com>
2025-01-20 23:03:29 -0500

    test

--- Backend/routes/webhook_routes.py
+++ Backend/routes/webhook_routes.py
@@ -4,12 +4,15 @@ webhook_bp = Blueprint('webhook', __name__)
 
 @webhook_bp.route('/webhook', methods=['POST'])
 def git_update():
-    repo = git.Repo('../../')
-    origin = repo.remotes.origin
-    repo.create_head('main',
-                     origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
-    origin.pull()
-    return '', 200
+    try:
+        repo = git.Repo('../../')
+        origin = repo.remotes.origin
+        repo.create_head('main',
+                        origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
+        origin.pull()
+        return 'Pull successful', 200
+    except Exception as e:
+        return f'Error: {e}', 500
     # payload = request.get_json()
     # # Process the payload
     # return jsonify({"status": "success"}), 200
