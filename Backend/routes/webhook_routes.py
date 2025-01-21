a98306ca38e4699edbf762c51031f482154e1f94
jesse <jessekarwaski@hotmail.com>
2025-01-20 22:53:28 -0500

    f

--- Backend/routes/webhook_routes.py
+++ Backend/routes/webhook_routes.py
@@ -4,12 +4,12 @@ webhook_bp = Blueprint('webhook', __name__)
 
 @webhook_bp.route('/webhook', methods=['POST'])
 def git_update():
-    # repo = git.Repo('./DnD-Combat-app')
-    # origin = repo.remotes.origin
-    # repo.create_head('main',
-    #                  origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
-    # origin.pull()
-    # return '', 200
-    payload = request.get_json()
-    # Process the payload
-    return jsonify({"status": "success"}), 200
+    repo = git.Repo('./DnD-Combat-app')
+    origin = repo.remotes.origin
+    repo.create_head('main',
+                     origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
+    origin.pull()
+    return '', 200
+    # payload = request.get_json()
+    # # Process the payload
+    # return jsonify({"status": "success"}), 200
